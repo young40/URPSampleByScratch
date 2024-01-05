edb528e3ebfa0848f9a2a34a8dcf85931b0f8f1c
Bourne Young <young40@gmail.com>
2024-01-05 23:07:19 +0800

    update 3D skybox

---
+++ Assets/CameraStacking/3DSkybox/SkyBoxCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxCamera : MonoBehaviour
+{
+    private Camera mainCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = mainCamera.transform.rotation;
+    }
+}
